8968cb1f1c7fa356abdc46a2d61d4c7e2cc35332
Paul Giovanni <it@raritangroup.com>
2026-05-05 21:43:23 -0400

    feat: add AddChatLevel migration for chat_level column on player_characters

---
+++ peeposredemption.Infrastructure/Migrations/20260505234813_AddChatLevel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace peeposredemption.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChatLevel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "chat_level",
+                table: "player_characters",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "chat_level",
+                table: "player_characters");
+        }
+    }
+}
